8d1c6331d16a792a0f7b2a8fa63e8fde7e155802
Eduardo Oliveira <eduardobolive@gmail.com>
2022-07-21 20:51:42 -0300

    Criação das cores e estilos de texto padrão

---
+++ lib/app/pages/util/app_color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color background = Color(0xffFFFFFF);
+  static const Color defaultColor = Color(0xff5A66EA);
+  static const Color secundaryColor = Color(0xff6CBAA2);
+  static const Color inputLabelColor = Color(0xffACA9BB);
+  static const Color white = Color(0xffffffff);
+
+  // Primeira versão
+  // static const Color background = Color(0xffE7F6D1);
+  // static const Color defaultColor = Color(0xff005165);
+  // static const Color secundaryColor = Color(0xffA8BF89);
+  // static const Color inputLabelColor = Color(0xffCBD9B8);
+  // static const Color white = Color(0xffffffff);
+}
