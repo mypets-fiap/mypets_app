fbc101cbbdcda1846295be65b54d132fb0d459ff
Eduardo Oliveira <eduardobolive@gmail.com>
2022-07-21 21:23:00 -0300

    Ocultando barra de status e barra de botões nativa

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mypets/app/pages/login_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -10,6 +13,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.immersiveSticky,
+    );
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
