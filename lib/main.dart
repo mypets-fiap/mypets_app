4771b5a053a4cf421d23e9ad35d92966cc6c00c7
Eduardo Oliveira <eduardobolive@gmail.com>
2022-07-26 22:35:31 -0300

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:flutter/services.dart';
 import 'package:mypets/app/pages/home_page.dart';
 import 'package:mypets/app/pages/login_page.dart';
-=======
-import 'app/pages/login_page.dart';
-import 'firebase_options.dart';
->>>>>>> 06971d7a8f7f64514f510e3586a9bedb68cb902c
+import 'package:mypets/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     SystemChrome.setEnabledSystemUIMode(
       SystemUiMode.immersiveSticky,
     );
@@ -30,13 +24,8 @@ class MyApp extends StatelessWidget {
       initialRoute: "/",
       routes: {
         "/": (context) => const LoginPage(),
-        "/homePage": (context) => HomePage(),
+        "/homePage": (context) => const HomePage(),
       },
-=======
-    return MaterialApp(
-      title: 'Flutter auth Demo',
-      home: LoginPage(),
->>>>>>> 06971d7a8f7f64514f510e3586a9bedb68cb902c
     );
   }
 }
