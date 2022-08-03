ede2b4040b6b0aa9ba55d9f3f5b37ecefc2a6db5
Henrique Renan Chang <henriquerenanchang@MacBook-Air-de-Henrique-108.local>
2022-08-02 22:00:16 -0300

    reminders

---
+++ lib/app/components/geral/reminders_table.dart
@@ -0,0 +1,66 @@
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../../model/event_pet.dart';
+import '../../pages/util/app_color.dart';
+import '../../pages/util/app_text_style.dart';
+
+class RemindersTable extends StatelessWidget {
+
+  const RemindersTable({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return SizedBox(
+      height: 400,
+      child: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(bottom: 15),
+            child: Text("Agenda", style: AppTextStyle.section),
+          ),
+          _createDataTableReminders()
+        ],
+      ),
+    );
+  }
+
+  DataTable _createDataTableReminders() {
+    return DataTable(
+        decoration: BoxDecoration(
+          border: Border.all(color: AppColor.secundaryColor, width: 1,style: BorderStyle.solid),
+            borderRadius: BorderRadius.circular(10.0)
+        ),
+        headingRowColor: MaterialStateColor.resolveWith((states) => AppColor.backgroudColor),
+        dataRowColor: MaterialStateColor.resolveWith((states) => AppColor.backgroudColor),
+        columns: _createColumns(),
+        rows: _createRows());
+  }
+
+  List<DataColumn> _createColumns() {
+    return const [
+      DataColumn(label: Text('Data', style: AppTextStyle.headerTable)),
+      DataColumn(label: Text('Compromisso', style: AppTextStyle.headerTable)),
+      DataColumn(label: Text('Pet', style: AppTextStyle.headerTable))
+    ];
+  }
+
+  List<DataRow> _createRows() {
+    final DateFormat formatter = DateFormat('dd/MM/yyyy');
+    List scheduleList = [];
+    scheduleList.add(EventPet(DateTime.now(), "Dentista", "Rodolfo"));
+    scheduleList.add(EventPet(DateTime.now(), "Banho", "Sheilo"));
+    scheduleList.add(EventPet(DateTime.now(), "Cirurgia", "Piggy"));
+
+    return [
+      for(var rowData in scheduleList)
+        DataRow(cells: [
+          DataCell(Text(formatter.format(rowData.data), style: AppTextStyle.dataTable)),
+          DataCell(Text(rowData.compromisso, style: AppTextStyle.dataTable)),
+          DataCell(Text(rowData.pet, style: AppTextStyle.dataTable)),
+        ])
+    ];
+  }
+}
