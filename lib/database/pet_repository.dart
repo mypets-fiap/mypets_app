0dd0184582af1c0be97ec38b8be2104e4aa5c77d
Gustavo Bezerra <gustavobz305@gmail.com>
2022-08-19 18:15:52 -0300

    Adicionando busca de pets

--- lib/database/pet_repository.dart
+++ lib/database/pet_repository.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:mypets/model/pet.dart';
 
 class PetRepository {
@@ -8,5 +9,11 @@ class PetRepository {
     // Add a new document with a generated ID
     return db.collection("pets").add(pet.toJson()).then((DocumentReference doc) =>
         print('DocumentSnapshot added with ID: ${doc.id}'));
+  }
+
+   Future getPet() async {
+    return await db.collection("pets").doc("ujVbwCTnkAl43QQnswMB").get().then((querySnapshot) {
+      print(querySnapshot.data());
+    });
   }
 }
