4fd466afbdbfdb54cd7c286755160d576e6aa495
Eduardo Oliveira <eduardobolive@gmail.com>
2022-08-22 21:51:23 -0300

    Correção para AssetImage e BoxFit.cover

--- lib/app/components/card/item_card.dart
+++ lib/app/components/card/item_card.dart
@@ -4,14 +4,13 @@ import 'package:mypets/model/servico.dart';
 import '../../pages/util/app_color.dart';
 import '../../pages/util/app_text_style.dart';
 
-class ItemCard extends StatelessWidget{
-
+class ItemCard extends StatelessWidget {
   final Servico servico;
 
   const ItemCard({
     Key? key,
     required this.servico,
-  }): super(key:key);
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,23 +18,19 @@ class ItemCard extends StatelessWidget{
       children: [
         Expanded(
             child: Container(
-              padding: EdgeInsets.all(1),
-              decoration: BoxDecoration(
-                  color: AppColor.secundaryColor,
-                  borderRadius: BorderRadius.circular(10)
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image(
-                  image: NetworkImage(servico.imagemPath),
-                ),
-              )
-            )
-        ),
-        Text(servico.nome,style: AppTextStyle.petName)
+                padding: const EdgeInsets.all(1),
+                decoration: BoxDecoration(
+                    color: AppColor.secundaryColor,
+                    borderRadius: BorderRadius.circular(10)),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image(
+                    image: AssetImage(servico.imagemPath),
+                    fit: BoxFit.cover,
+                  ),
+                ))),
+        Text(servico.nome, style: AppTextStyle.petName)
       ],
     );
   }
 }
-
-
